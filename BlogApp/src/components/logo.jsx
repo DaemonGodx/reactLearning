e5ef0b12318e857250e9194eb3fcdf5acf24d186
DaemonGodx <pritam.2024ug@iiitranchi.ac.in>
2025-12-22 14:12:53 +0530

    all done

--- BlogApp/src/components/logo.jsx
+++ BlogApp/src/components/logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width='100px'}) {
   return (
     <div>
-      Logo
+      <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="logo lost"  className="h-10" />
     </div>
   )
 }
--- BlogApp/src/pages/Home.jsx
+++ BlogApp/src/pages/Home.jsx
@@ -1,31 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import appwriteService from "../appwrite/db_config";
-import {Container, PostCard} from '../components'
+import { Container, PostCard } from '../components'
+import authService from '../appwrite/auth';
+import { useSelector } from 'react-redux';
 
 function Home() {
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        appwriteService.getPosts().then((res) => {
-            if (res?.documents) {
-                setPosts(res.documents)
-            } else {
-                setPosts([])
-            }
-        })
-    }, [])
-  
-    if (!posts || posts.length === 0) {
+    const [loading, setLoading] = useState(true)
+    const [user, setUser] = useState(null)
+    const userData = useSelector(state => state.auth.userData)
+    if (!userData) {
         return (
             <div className="w-full py-8 mt-4 text-center">
                 <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
-                                Login to read posts
-                            </h1>
-                        </div>
-                    </div>
+                    <h1 className="text-2xl font-bold hover:text-gray-500">
+                        Login to read posts
+                    </h1>
                 </Container>
             </div>
         )
@@ -34,13 +23,14 @@ function Home() {
     return (
         <div className='w-full py-8'>
             <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
+                <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            Welcome back 👋
+          </h1>
+          <p className="text-gray-600">
+            You are logged in successfully.
+          </p>
+        </div>
             </Container>
         </div>
     )
